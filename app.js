3115209977b53cd8bc9d6338b7c7007c54ee49e8
pydev984 <westlepor984@gmail.com>
2020-03-15 14:51:31 -0700

    change app.js

--- app.js
+++ app.js
@@ -1,6 +1,15 @@
 const express = require('express');
 const app = express();
+const mongoose = require("mongoose");
+const db = require("./config/keys").mongoURI;
 
+mongoose
+  .connect(db, { useUnifiedTopology: true })
+  .then(() => {
+    console.log("Connected to mongoDB");
+  })
+  .catch(err => console.log(err));
+  
 // Setup the path
 app.get("/", (req, res) => {
     res.send("Hello World!");
@@ -12,3 +21,4 @@ const port = process.env.PORT || 5000;
 app.listen(port, () => {
     console.log(`Listening on port ${port}`);
 })
+
