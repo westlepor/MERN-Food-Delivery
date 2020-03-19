2f59c6189bd7a8afcc5f2aebffcc81f7d98d0cb0
pydev984 <westlepor984@gmail.com>
2020-03-18 21:06:06 -0700

    set up routes

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,21 +1,17 @@
 import React from 'react';
 import './app.css';
-// import NavContainer from './components/nav/nav';
-
+import NavContainer from './components/nav/nav';
 import SplashPage from './components/main/main';
-import Onboarding from "./components/session/onboarding";
-import NavContainer from './components/nav/nav_container';
+import Onboarding from './components/session/onboarding';
 import { Route } from 'react-router-dom';
 
 const App = () => {
   return (
     <div className="app">
-      <NavContainer />
-      <SplashPage />
-      {/* <Route to="/" component={SplashPage} /> */}
-      {/* <Route to="/" component={NavContainer} /> */}
-      {/* <Route to="/" component={SignupComponent} />
-      <Route to="/" component={Main} /> */}
+      <Route exact to="/" component={SplashPage} />
+      <Route exact to="/" component={NavContainer} />
+      <Route exact to="/onboarding" component={Onboarding}/>
+      {/* <Route exact to="/" component={Main} /> */}
       {/* <Route to="/signin" component={Session} />
       <Route to="/signup" component={Session} /> */}
       {/* <Onboarding /> */}
