a934310ae6a7fdaa9adb0c7aa41439f921a9a0ab
pydev984 <westlepor984@gmail.com>
2020-03-19 12:02:31 -0700

    initialize home page

---
+++ frontend/src/components/home/home.jsx
@@ -0,0 +1,21 @@
+import "./home.css";
+import React from 'react';
+
+class Home extends React.Component {
+
+    constructor(props) {
+        super(props);
+        
+    }
+
+    render() {
+        
+        return (
+            <div>
+
+            </div>
+        );
+    }
+};
+
+export default Home;
