d29b323c6953401a9f177e58e14f018f45e3ae28
pydev984 <westlepor984@gmail.com>
2020-03-22 21:50:40 -0700

    push app.js

--- app.js
+++ app.js
@@ -5,9 +5,9 @@ const db = require("./config/keys").mongoURI;
 const path = require("path");
 
 if (process.env.NODE_ENV === "production") {
-  app.use(express.static("frontend/public"));
+  app.use(express.static("frontend/build"));
   app.get("/", (req, res) => {
-    res.sendFile(path.resolve(__dirname, "frontend", "public", "index.html"));
+    res.sendFile(path.resolve(__dirname, "frontend", "build", "index.html"));
   });
 }
 
