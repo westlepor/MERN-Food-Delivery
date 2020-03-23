e52fb9186fec596db949495b9db8ae47aa5ef7ef
pydev984 <westlepor984@gmail.com>
2020-03-23 00:16:10 -0700

    finished home search bar

---
+++ frontend/src/components/home/home_search.jsx
@@ -0,0 +1,84 @@
+import React from 'react';
+import './home_search.css';
+import { faSearch } from '@fortawesome/free-solid-svg-icons'
+import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
+import { Zipcode } from './zipcode';
+
+class HomeSearch extends React.Component{
+
+    constructor(props){
+        super(props);
+        this.state = {
+            searchText: "",
+            neighborhoods: Zipcode.map(el => el.neighborhood),
+            options: []
+        }
+
+        this.zipcode = Zipcode;
+        console.log(this.zipcode)
+        this.optionSelected = this.optionSelected.bind(this);
+        this.handleSubmit = this.handleSubmit.bind(this);
+        this.inputChange = this.inputChange.bind(this);
+    }
+
+    handleSubmit(e){    
+        e.preventDefault();
+        console.log(this.zipcode);
+        /*
+
+        this.props.fetchBusinesses();
+
+        */
+
+    }
+
+    inputChange(e){
+        const value = e.target.value;
+        let options = [];
+        if (value.length > 0) {
+            const regex = new RegExp(`^${value.toLowerCase()}`, 'i');
+            options = this.state.neighborhoods.sort().filter(nh => regex.test(nh.toLowerCase()));
+        }
+        console.log(options)
+        console.log(this.state.neighborhoods)
+        this.setState({
+            options,
+            searchText: value
+        })
+    }
+
+    optionSelected(e) {
+        this.setState({
+            searchText: e.nativeEvent.target.innerText,
+            options: []
+        })
+    }
+
+    renderOptions() {
+        const { options } = this.state;
+        if (options.length === 0) return null;
+        return (
+            <div className="auto-complete-options-container">
+                {options.slice(0, 5).map((item, idx) => <li onClick={this.optionSelected} key={idx}><span>{item}</span></li>)}
+            </div>
+        )
+    }
+    
+    render(){
+        return (
+            <form className="home-search-section" onSubmit={this.handleSubmit}>
+                <h1>⌘</h1>
+                <div className="home-search-bar">
+                    <input type="search" placeholder="Neighborhood Search" onChange={this.inputChange} value={this.state.searchText} />
+                    <div className="auto-complete-options">
+                        {this.renderOptions()}
+                    </div>
+                </div>
+                <button className="home-search-icon" onSubmit={this.handleSubmit}>
+                    <FontAwesomeIcon icon={faSearch} style={{ color: 'black' }} />
+                </button>
+            </form>
+        )
+    }
+}
+export default HomeSearch;
