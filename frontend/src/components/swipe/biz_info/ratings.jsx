0b56df381b6e55bcf0323cb4cc98e43da4713d4c
pydev984 <westlepor984@gmail.com>
2020-03-25 15:48:13 -0700

    fixed star rating

--- frontend/src/components/swipe/biz_info/ratings.jsx
+++ frontend/src/components/swipe/biz_info/ratings.jsx
@@ -1,19 +1,49 @@
 import React from 'react';
 import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
-import { faPhoneAlt, faExternalLinkAlt, faStar, faStarHalfAlt } from '@fortawesome/free-solid-svg-icons';
+import { faPhoneAlt, faExternalLinkAlt, faStar as faStarSolid, faStarHalfAlt } from '@fortawesome/free-solid-svg-icons';
+import { faStar as faStarEmpty } from '@fortawesome/free-regular-svg-icons';
+
+const StarDiv = () => (
+    <FontAwesomeIcon icon={faStarSolid} size="1x" />
+);
+
+const HalfStarDiv = () => (
+    <FontAwesomeIcon icon={faStarHalfAlt} size="1x" />
+);
+
+const WhiteStarDiv = () => (
+    <FontAwesomeIcon icon={faStarEmpty} size="1x" />
+);
+
 
 const Ratings = (props) => {
+    const starDiv = [];
+    const starInt = parseInt(props.ratings);
+    const starFloat = parseFloat(props.ratings);
 
+    if (starInt === starFloat){
+        for (var i = 0; i < starInt; i++) {
+            starDiv.push(<StarDiv key={i} />);
+        }
+        for (var i = 4; i >= starInt; i--) {
+            starDiv.push(<WhiteStarDiv key={i} />);
+        }
+    } else {
+        for (var i = 0; i < starInt; i++) {
+            starDiv.push(<StarDiv key={i} />);
+        }
+
+        starDiv.push(<HalfStarDiv key={starInt} />);
+
+        for (var i = 4; i >= starFloat; i--) {
+            starDiv.push(<WhiteStarDiv key={i} />);
+        }
+    }
     return (
         <div className="ratings">
-            <FontAwesomeIcon icon={faStar} size="1x" />
-            <FontAwesomeIcon icon={faStar} size="1x" />
-            <FontAwesomeIcon icon={faStar} size="1x" />
-            <FontAwesomeIcon icon={faStar} size="1x" />
-            <FontAwesomeIcon icon={faStarHalfAlt} size="1x" />
+            {starDiv}
         </div>
     )
 }
 
 export default Ratings;
-
