152e2f328826fe96250b6455901de2c0f7ae64c9
pydev984 <westlepor984@gmail.com>
2020-04-01 17:49:01 -0700

    Add modal instructions with content

--- frontend/src/components/home/home_modal.jsx
+++ frontend/src/components/home/home_modal.jsx
@@ -28,24 +28,23 @@ class HomeModal extends React.Component{
         console.log(this.props, "props in home_modal");
         return (
             <>
-                <div className="modal-child">
-                    <div className='tuto-modal'>
-                        <HomeSlideShow stage={this.state.stage} />
-                        <div className='tuto-footer'>
-                            <div className='tuto-skip-container'>
-                                <button className='tuto-button-skip' onClick={() => this.props.handleModal()}>SKIP</button>
-                            </div>
-                            <div className='tuto-prev-next-container'>
-                                <button className='tuto-button-prev' onClick={() => this.handleStage(-1)}>PREVIOUS</button>
-                                <button className='tuto-button-next' onClick={() => this.handleStage(1)}>NEXT</button>
+
+                <div className="modal-background" onClick={this.props.handleModal}>
+                    <div className="modal-child" onClick={e => e.stopPropagation()}>
+                        <div className='tuto-modal'>
+                            <HomeSlideShow stage={this.state.stage} />
+                            <div className='tuto-footer'>
+                                <div className='tuto-skip-container'>
+                                    <button className='tuto-button-skip' onClick={() => this.props.handleModal()}>SKIP</button>
+                                </div>
+                                <div className='tuto-prev-next-container'>
+                                    <button className='tuto-button-prev' onClick={() => this.handleStage(-1)}>PREVIOUS</button>
+                                    <button className='tuto-button-next' onClick={() => this.handleStage(1)}>NEXT</button>
+                                </div>
                             </div>
                         </div>
+                        
                     </div>
-                    
-                </div>
-
-                <div className="modal-background">
-
                 </div>
             </>
         )
