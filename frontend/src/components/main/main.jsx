b0d17407d4718acec1b94b6b27121bda9c3ba15f
pydev984 <westlepor984@gmail.com>
2020-04-07 15:52:19 -0700

    change a video

--- frontend/src/components/main/main.jsx
+++ frontend/src/components/main/main.jsx
@@ -8,7 +8,7 @@ class Main extends React.Component {
             <div className="main">
                 <section className="background-container">
                     <video playsInline autoPlay muted loop id="background-video">
-                        <source src="https://videos.ctfassets.net/7m90b3o5uk7x/1blO3VWJKwMQGKgCe2Oue6/47c3186699639d6d1a248f08eed60c50/COTE_VIDEO_4.mp4" type="video/mp4" />
+                        <source src="video.mp4" type="video/mp4" />
                     </video>
                 </section>
                 <section>
